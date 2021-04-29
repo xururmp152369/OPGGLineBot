c8cc84b29fdd0f250af490af7dfc7f13b2282034
xururmp152369 <xururmp152369@gmail.com>
2021-04-29 15:59:12 +0800

    detail test

---
+++ LineBotDBClass/Models/ChampionJunInfo.cs
@@ -0,0 +1,24 @@
+﻿using LinBotDBClass.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace LineBotDBClass.Models
+{
+    public class ChampionJunInfo
+    {
+        [Key]
+        public int Id { get; set; }
+        public string CpLane { get; set; }
+        public string CpName { get; set; }
+        public string CpTier { get; set; }
+        public string CpSummonerSkill { get; set; }
+        public string CpSkill { get; set; }
+        public string CpStartItem { get; set; }
+        public string CpCoreItem1 { get; set; }
+        public string CpCoreItem2 { get; set; }
+        public string CpBoot1 { get; set; }
+        public string CpBoot2 { get; set; }
+        public string CpRune { get; set; }
+
+        public virtual ChampionState ChampionState { get; set; }
+    }
+}
